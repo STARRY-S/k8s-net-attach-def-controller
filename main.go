b0cf5e2deae4eebf3ef67b85ee64959b5d7cd09f
niusmallnan <niusmallnan@gmail.com>
2022-04-07 09:57:46 +0800

    Bump k8s resync period 30s

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ var (
 	kubeconfig string
 
 	// defines default resync period between k8s API server and controller
-	syncPeriod = time.Second * 5
+	syncPeriod = time.Second * 30
 )
 
 func main() {
